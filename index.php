a931ac6d8222775d5cd4138a13b0bdabe7227bfe
dim2k2006 <dim2k2006@yandex.ru>
2014-08-02 20:10:05 +0300

    geekon

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<meta http-equiv="pragma" content="no-cache">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+		<meta name="Description" content="">
+		<meta name="Keywords" content="">
+		<!--[if IE 6]><html lang="en" class="ie6"><![endif]--> 
+		<!--[if IE 7]><html lang="en" class="ie7"><![endif]--> 
+		<!--[if IE 8]><html lang="en" class="ie8"><![endif]--> 
+		<!--[if IE 9]><html lang="en" class="ie9"><![endif]--> 
+		<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700&subset=cyrillic,latin' rel='stylesheet' type='text/css'>
+		<link href="css/style.css" type='text/css' rel='stylesheet'>
+		<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+		<script src="js/jquery-ui.min.js"></script>
+		<link rel="shortcut icon" href="" type="image/x-icon">
+		<title></title>
+	</head>
+	<body>
+		<div class="header">
+			<div>
+				<a href="#">Geekon<br>Systems</a>
+			</div>
+			<span>
+				<a href="#"></a>
+			</span>
+			<ul class="navigation">
+				<li><a class="toservices" href="#services">Услуги</a></li>
+				<li><a class="toprojects" href="#projects">Работы</a></li>
+				<li><a class="tocontacts" href="#contacts">Контакты</a></li>
+			</ul>
+		</div>
+		
+		<div class="intro">
+			<ul>
+				<li class="desk">
+					<div></div>
+					<p>< html ></p>
+				</li>
+				<li class="key">
+					<div></div>
+				</li>
+			</ul>
+			<h3><span>< <i>Разработка</i> ></span> быстрых, современных, адаптивных <span>< / <i>cайтов</i> ></span></h3>
+			<div>
+				<a class="toservices" href="#services">Узнайте о нас больше</a>
+			</div>
+		</div>
+		
+		<div class="services">
+			<div id="services"></div>
+			<h1>Услуги</h1>
+			<i></i>
+			<h4>Есть свой бизнес, но нет своего сайта? <strong>Geekon Systems</strong> - это первоклассный сервис, профессионализм и конечно самые низкие цены. Подобно геккону мы адаптируемся к любой среде и находим подход к любому клиенту, так как свою работы мы выполняем исключительно со страстью гиков - людей,  чрезмерно увлечёнными чем либо, а именно созданием первоклассных сайтов.</h4>
+			<ul>
+				<li class="crossbrowser"><h2>Кроссбраузерность</h2><p>Свойство сайта отображаться и работать во всех популярных браузерах идентично. Также ваш сайт будет отлично работать и в старых версиях браузеров, например, Internet Explorer 6+.</p></li>
+				<li class="code"><h2>Валидность кода</h2><p>Сайт, который строго следует веб стандартам корректно отображается не только на стационарных ПК и ноутбуках, но и на множестве мобильных устройств с самым разным разрешением.</p></li>
+				<li class="optimization"><h2>Оптимизация</h2><p>Оптимизация всех составляющих вашего сайта, начиная от кода самих страничек и заканчивая всевозможными картинками, скриптами, стилями и прочими элементами.</p></li>
+			</ul>
+			<div class="cb"></div>
+			<ul>
+				<li class="speed"><h2>Скорость</h2><p>Оптимизация скорости загрузки сайта, что позволит быстро получить доступ к контенту, сделав ваш сайт максимально эффективным.</p></li>
+				<li class="responsive"><h2>Адаптивность</h2><p>При малой ширине экрана изменяется взаимное расположение функциональных блоков сайта, без появления горизонтальной полосы прокрутки.</p></li>
+				<li class="wordpress"><h2>Cms Wordpress</h2><p>При необходимости управления содержимым вашего сайта, есть возможность установки системы управления контентом Wordpress.</p></li>
+			</ul>
+			<div class="cb"></div>
+		</div>
+		
+		<div class="projects">
+			<p id="projects"></p>
+			<h1>Работы</h1>
+			<i></i>
+			<div class="right">
+				<ul>
+					<li class="screen"><img src="images/gt_mp.png" alt=""></li>
+					<li class="desc">
+						<h5>Green Tour</h5>
+						<p>Green Tour - это компания предоставляющая услуги в сфере туризма и отдыха главным ориентиром деятельности которой является предоставление свежих туристических решений.</p>
+						<a href="http://green-tour.by/" target="_blank">green-tour.by<u></u></a>
+						<span>UI, Website, Front-End</span>
+					</li>
+				</ul>
+				<p class="cb"></p>
+			</div>
+			
+			<div class="left">
+				<ul>
+					<li class="screen"><img src="images/ev_mp.png" alt=""></li>
+					<li class="desc">
+						<h5>Служба эвакуации БУКСИР</h5>
+						<p>Служба эвакуации автомобилей БУКСИР - это комплекс услуг по поиску ближайшего от Вас эвакуатора для погрузки, буксировки, перевозки и доставки Вашего транспортного средства до места назначения.</p>
+						<a href="http://evacuator.vitebsk.by/" target="_blank"><u></u>evacuator.vitebsk.by</a>
+						<span>UI, Website, Responsive, Front-End</span>
+					</li>
+				</ul>
+				<p class="cb"></p>
+			</div>
+			
+			<div class="right">
+				<ul>
+					<li class="screen"><img src="images/gtp_mp.png" alt=""></li>
+					<li class="desc">
+						<h5>BTL-агентство GREEN TOWN</h5>
+						<p>Грин Таун это комплекс услуг в сфере рекламы и коммуникации. BTL-агентство Green Town уже на протяжении пяти лет занимается активным продвижением товаров и услуг своих клиентов.</p>
+						<a href="http://www.gtpromo.by/" target="_blank">gtpromo.by<u></u></a>
+						<span>UI, Website, CMS, Responsive, Front-End, Back-End</span>
+					</li>
+				</ul>
+				<p class="cb"></p>
+			</div>
+			
+			<div class="left">
+				<ul>
+					<li class="screen"><img src="images/ta_mp.png" alt=""></li>
+					<li class="desc">
+						<h5>Technoadvice</h5>
+						<p>Technoadvice.ru - это информационный портал содержащий советы по выбору аудио-видео техники, крупной и мелкой бытовой техники, встраиваемой техники, ноутбуков, компьютеров и телефонов.</p>
+						<a href="http://www.technoadvice.ru/" target="_blank"><u></u>technoadvice.ru</a>
+						<span>UI, Website, CMS, Front-End, Back-End</span>
+					</li>
+				</ul>
+				<p class="cb"></p>
+			</div>
+			
+			<div class="right last">
+				<ul>
+					<li class="screen"><img src="images/sz_mp.png" alt=""></li>
+					<li class="desc">
+						<h5>Срочные займы</h5>
+						<p>Срочные займы - это сайт для быстрорастущей и динамично развивающейся компании занимающейся предоставлением микрокредитов населению.</p>
+						<a href="http://s-z.by/" target="_blank">s-z.by<u></u></a>
+						<span>UI, Website, Front-End</span>
+					</li>
+				</ul>
+				<p class="cb"></p>
+			</div>
+		</div>
+		
+		<div class="contacts">
+			<p id="contacts"></p>
+			<h1>Контакты</h1>
+			<i></i>
+			<h4>Нужен первоклассный сайт за разумные деньги? Свяжитесь с нами в любое удобное для Вас время!</h4>
+			
+			<div class="author">
+				<span>Малофеев Дмитрий Сергеевич</span>
+				<p>Разработчик</p>
+				<p>+375 33 329 35 22</p>
+				<a href="mailto:creativemail@geekon.by">creativemail@geekon.by</a>
+				<a href="skype:dim2k_by1?chat">skype: dim2k_by1</a>
+				<a href="">Вконтакте</a>
+			</div>
+			<div class="form">
+				<form action="" method="POST">	
+					<span>Имя: *</span>
+					<input type="text" name="name" id="name" value="" tabindex="1">
+					<span>E-mail: *</span>
+					<input type="text" name="email" id="email" value="" tabindex="2">
+					<span>Тема: *</span>
+					<input type="text" name="subject" id="subject" value="" tabindex="3">
+					<span>Сообщение: *</span>
+					<textarea name="text_message" id="text_message" rows="13" cols="30" tabindex="4"></textarea>
+					<button type="submit" tabindex="5">Отправить</button>
+					<input name="usercheck" id="usercheck" type="text" size="1" style="visibility:hidden;" value="">
+				</form>
+			</div>
+			<p class="cb"></p>
+		</div>
+		
+		<div class="footer">
+			<p>© <span>2013 — 2014</span> Geekon Systems. Разработка сайтов.</p>
+		</div>
+		
+		<script type="text/javascript">
+			menuDropInProgress = false;
+			pageScrollInProgress = false;
+		
+			$(document).ready(function (){
+				$('.header span a').click(function(event) {
+					event.preventDefault();
+					if (!menuDropInProgress) {
+						menuDropInProgress = true;
+						$('.header .navigation li').slideToggle(200).css('display','block');
+						$('.header span a').toggleClass('activeMenu');
+						setTimeout(function() {menuDropInProgress = false;}, 600)
+					}
+				});
+				
+				
+				setInterval(function() {
+					$('.intro div a').css({'top':'0', 'width':'100%'});
+					$('.intro div a').stop(true, true).effect( "bounce", {direction:'up', distance: 15, times: 3 }, 1900);
+				}, 4000);
+				
+				
+				$('.navigation li a, .intro div a').click(function(event) {
+					event.preventDefault();
+					if (!pageScrollInProgress) {
+						pageScrollInProgress = true;
+						$('html, body').animate({scrollTop: $('#'+$(this).attr('class').replace("to", "")).offset().top}, 'slow');
+						setTimeout(function() {pageScrollInProgress = false;}, 600)
+					}
+				});
+			});
+	</script>
+	</body>
+</html>
